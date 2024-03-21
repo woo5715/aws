e7bb98105dc3986983ef7b59efebdb44293c84c6
woo5715 <too5715@naver.com>
2024-03-21 11:46:01 +0900

    yee

---
+++ src/test/java/com/example/aws_ecs/AwsEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_ecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
